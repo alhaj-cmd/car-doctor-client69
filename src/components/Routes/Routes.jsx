be38bb4a45f1585d9b79e100cbbd96b727563d50
github_username <you@domain.com>
2023-05-13 16:56:22 +0600

    first commit

---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SingUp from "../../Pages/SignUp/SingUp";
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'login',
+                element:<Login></Login>
+            },
+            {
+                path:'signup',
+                element:<SingUp></SingUp>
+            }
+        ]
+    }
+]);
+
+export default router;
